7b3e3fbb1348f869718bc715ed1f7692e24ea8af
DmitriiVP <dm1trymkv@yandex.ru>
2025-03-04 13:03:20 +0300

    6 (very final version with otchetes)

--- main.cpp
+++ main.cpp
@@ -9,34 +9,43 @@ using namespace KDM;
 
 #pragma region функции-заглушки
 int f1() {
+    cout << "function f1 is running...\n\n";
+    return 1;
+}
+int f2() {
+    cout << "function f2 is running...\n\n";
+    return 2;
+}
+int f3() {
+    cout << "function f3 is running...\n\n";
+    return 3;
+}
+int f4() {
     cout << "Здравствуйте!" << endl;
     return 1;
 }
 
-int f2() {
+int f5() {
     cout << "Логарифм 49: \t" << log(49) << endl;
     return 2;
 }
 
-int f3() {
-    cout << "До свидания!" << endl;
-    return 3;
-}
 
 int f0() {
     cout << "Работа программы завершена" << endl;
     return 0;
 }
 
-// #pragma endregion
+#pragma endregion
 
-const int ITEMS_NUMBER = 4;
+const int ITEMS_NUMBER = 6;
 
 
 int main() {
-    MenuItem items[ITEMS_NUMBER] {MenuItem{"Поздороваться с пользователем", f1}, MenuItem{"Вычислить логарифм 49", f2}, MenuItem{"Попрощаться с пользователем", f3}, MenuItem{"Выход", f0}};
+    MenuItem items[ITEMS_NUMBER] {MenuItem{"first item", f1}, 
+    MenuItem{"second item", f2}, 
+    MenuItem{"third item", f3}, MenuItem{"fourth item", f4}, MenuItem{"fifth item", f5}, MenuItem{"exit", f0}};
     MyMenu menu("Меню", items, ITEMS_NUMBER);
     while (menu.runCommand()) {};
-
     return 0;
 }
